be4736d581cbc43e744126ac76ad26c60bd235a9
Lance Jenkin <lancejenkin@gmail.com>
2012-02-10 11:39:41 +0200

    Added Measurement and SignalDb files

---
+++ src/Measurement.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+""" Provides a means to preform a measurement.  Uses Signal Generate to generate
+    a signal, and the Audio Interface to play the signal and record the
+    reponse.
+
+
+"""
+
+import logging
+
+__author__ = "Lance Jenkin"
+__email__ = "lancejenkin@gmail.com"
+
+class Measurement(object):
+    pass
---
+++ src/SignalDb.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+""" Provides an interface to save signals, as well as the settings used to
+    create the signal, as well as the settings used to analyze the signal.
+"""
+
+
